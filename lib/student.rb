24526c01a16f74608164778b562c1a33e67bd864
KHong <albtrossfacefall@gmail.com>
2019-08-29 03:43:59 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+   attr_reader :knowledge
+
+   def initialize
+      @knowledge=[]
+   end
+
+   def learn(string)
+      @knowledge.push(string)
+   end
+
+
 
 end
